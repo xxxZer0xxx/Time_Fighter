4e3e297be0de1e735cd92b778a98dbcf81972235
xxxZer0xxx <max.reed@hotmail.com>
2020-02-23 01:02:22 +0100

    added CountDowntimer and resetGame

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -2,12 +2,20 @@ package com.raywenderlich.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     internal var score: Int = 0
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal val initialCountDown: Long = 60000
+    internal val countDownIntervall: Long = 1000
+
 
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreText: TextView
@@ -25,11 +33,39 @@ class MainActivity : AppCompatActivity() {
         tapMeButton.setOnClickListener{ view ->
             incrementScore()
         }
+
+        resetGame()
     }
 
     private fun incrementScore() {
+        if(!gameStarted) {
+            countDownTimer.start()
+            gameStarted = true
+        }
+
         score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreText.text = newScore
+    }
+
+    private fun resetGame() {
+        score = 0
+
+        gameScoreText.text = getString(R.string.yourScore, score)
+
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftText.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownIntervall) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftText.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+        gameStarted = false
     }
 }
